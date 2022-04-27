1e72ef86082de634578814d64026dea1515b8427
kiruba14112000 <kirubalovely2@gmail.com>
2022-04-27 18:23:01 +0530

    kiruba

---
+++ src/org/emp/Employee.java
@@ -0,0 +1,15 @@
+package org.emp;
+
+public class Employee {
+	private void empName() {
+		System.out.println("Id Num 1234");
+	}
+	private void empId() {
+		System.out.println("Nmae kirubakaran");
+	}
+public static void main(String[] args) {
+	Employee ab =new Employee();
+	ab.empId();
+	ab.empName();
+}
+}
